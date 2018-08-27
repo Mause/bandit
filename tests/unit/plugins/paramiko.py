8fd04bc6ce58c1891afe2c70626ff77d77f7bbd9
Dominic May <me@mause.me>
2018-08-27 16:39:12 +1000

    Correct more lint issues

--- tests/unit/plugins/paramiko.py
+++ tests/unit/plugins/paramiko.py
@@ -1,9 +1,24 @@
-import bandit
+# -*- coding:utf-8 -*-
+#
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-from . import example
-from . import example_file
+from tests.unit.plugins import example
+from tests.unit.plugins import example_file
 
 from unittest import TestCase
+
 
 class TestThing(TestCase):
     @example('''
@@ -13,7 +28,7 @@ class TestThing(TestCase):
     def test_it(self, results):
         self.assertEqual(len(results), 1)
 
-    @example_file(f'examples/paramiko_injection.py')
+    @example_file('examples/paramiko_injection.py')
     def test_paramiko_injection(self, results):
         self.assertEqual(len(results), 2)
         self.assertEqual(results[0]['test_id'], 'B601')
